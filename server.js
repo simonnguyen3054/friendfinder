0c223731c64904abb9808800fc64b50dd9038505
Simon Nguyen <simonnguyen3054@gmail.com>
2018-08-22 08:10:07 -0700

    add survey route

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var app = express();
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+var path = require("path");
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.static("public"));
+app.use(methodOverride('_method'));
 
+app.get('/survey', function(req, res){
+	res.sendFile(path.join(__dirname, "public/survey.html"));
+});
 
 app.listen(3000, function(){
 	console.log('listening on 3000');
